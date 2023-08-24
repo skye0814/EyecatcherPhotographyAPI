e63b804cc2935d1ad0d9510b227c9fe8f87cc6e1
John Matthew Cruz Arquelola <arquelolajm@gmail.com>
2023-08-24 18:48:49 +0800

    Added scripts for AWS CodeDeploy

---
+++ deploy-scripts/before_install.sh
@@ -0,0 +1,3 @@
+# remove installed code and the systemd service file
+rm -rf /var/www/*
+rm -rf /etc/systemd/system/webapi.service
---
+++ deploy-scripts/start_app.sh
@@ -0,0 +1,3 @@
+# enable and start systemd service 
+systemctl stop webapi.service 
+systemctl start webapi.service
