0cc6effe26667aa69f5e3e17470517a93e6a67b0
John Matthew Cruz Arquelola <arquelolajm@gmail.com>
2023-09-10 20:11:21 +0800

    Prevents pageNumber and pageSize to fall below 1

--- Core/WebModel/Request/PaginationFilterRequest.cs
+++ Core/WebModel/Request/PaginationFilterRequest.cs
@@ -5,13 +5,18 @@ namespace Core.WebModel.Request
     public class PaginationFilterRequest : IEntity
     {
         private int _pageSize;
+        private int _pageNumber;
 
         public bool isAscending { get; set; } = true;
-        public int PageNumber { get; set; } = 1;
+        public int PageNumber
+        {
+            get => _pageNumber;
+            set => _pageNumber = (value < 1) ? 1 : value;
+        }
         public int PageSize
         {
             get => _pageSize;
-            set => _pageSize = (value < 2) ? 2 : value;
+            set => _pageSize = (value < 1) ? 1 : value;
         }
         public string? Search {get; set;} = string.Empty;
         public string SortBy {get; set;} = string.Empty;
